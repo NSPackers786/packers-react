44cbf65d73af4f9340aa710b0381703e37909a7e
NSPackers786 <NSPackersandmovers78@gmail.com>
2025-01-09 09:02:44 +0530

    contact  changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -143,7 +143,7 @@ const Contact = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/submit-contact-form', {
+      const response = await fetch('http://localhost:5000/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
